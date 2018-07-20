c73d886e095c1cf07e99e63c1317cd15aac3f7b3
Shivain Thapar <shivain27@gmail.com>
2018-07-20 16:41:39 -0400

    Starting authentication functionality

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const express = require('express');
 // using require cause node js only has support for commonjs modules, not es2015 modules
 // import will be used for the front end (react etc)
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
-// route handler
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+passport.use(new GoogleStrategy());
 
 // check if the underlying environment (eg:heroku) has declared a port for us to use
 const PORT = process.env.PORT || 5000;
---
+++ notes.md
@@ -0,0 +1,11 @@
+
+Authentication - Google Auth, PassportJS for OAuth
+Backend - express server
+Front end - react and redux
+Payment - stripe
+MongoDB
+
+#### Google OAuth + PassportJS
+
+
+
